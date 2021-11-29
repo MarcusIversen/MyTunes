c7cbe5f60b24b43b3337681776912a7cdcbea642
FrederikReigstad <Frederik9876@gmail.com>
2021-11-29 12:05:20 +0100

    Fxml Luncher

--- src/gui/Main.java
+++ src/gui/Main.java
@@ -1,12 +1,33 @@
 package gui;
 
 import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
 
-public class Main{
+import javax.swing.*;
 
-    public static void main(String[] args) {
+public class Main extends Application{
+
+    @Override
+    public void start(Stage primaryStage) throws Exception
+    {
+        Parent parent = FXMLLoader.load(getClass().getResource("MainMenu.fxml"));
+        primaryStage.setTitle("Converter of Fun");
+        Scene scene = new Scene(parent);
+        primaryStage.setScene(scene);
+        primaryStage.show();
+
 
     }
+
+    public static void main(String[] args)
+    {
+
+    }
+
 }
 
 
