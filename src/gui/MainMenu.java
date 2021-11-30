c44fca959370c400a7da7ff985f37c2069f178aa
FrederikReigstad <Frederik9876@gmail.com>
2021-11-30 22:31:58 +0100

    new play list button made

--- src/gui/MainMenu.java
+++ src/gui/MainMenu.java
@@ -1,4 +1,28 @@
 package gui;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
 public class MainMenu{
+
+    public Button NewPlaylist;
+
+    public void onBmiClicked(ActionEvent actionEvent) throws IOException {
+        Stage swich = (Stage) NewPlaylist.getScene().getWindow();
+        Parent parent = FXMLLoader.load(getClass().getResource("NewPlaylist .fxml"));
+        Scene scene = new Scene(parent);
+        swich.setScene(scene);
+
+
+
+
+    }
+
+
 }
