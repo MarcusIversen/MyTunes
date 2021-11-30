f4ce149a7e634c16f53db6e542e7612f5031e37b
MarcusIversen <91528331+MarcusIversen@users.noreply.github.com>
2021-11-30 10:43:41 +0100

    PauseButton

--- src/gui/Main.java
+++ src/gui/Main.java
@@ -14,7 +14,7 @@ public class Main extends Application{
     @Override
     public void start(Stage primaryStage) throws Exception
     {
-        Parent parent = FXMLLoader.load(getClass().getResource("NewPlaylist.fxml"));
+        Parent parent = FXMLLoader.load(getClass().getResource("MainMenu.fxml"));
         primaryStage.setTitle("myTunes");
         Scene scene = new Scene(parent);
         primaryStage.setScene(scene);
